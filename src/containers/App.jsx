de8fc1196ace0f4d0dd07c2b964c1c1c4f50c44d
Fernando <29582347+FernandoKash@users.noreply.github.com>
2019-11-30 18:05:09 -0600

    Initial commit

---
+++ src/containers/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
+import '../assets/styles/App.scss';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <Search />
+
+                <Categories title="Mi lista">
+                    <Carousel>
+                        <CarouselItem />
+                        <CarouselItem />
+                        <CarouselItem />
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+
+                <Categories title="Tendencias">
+                    <Carousel>
+                        <CarouselItem />
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+
+                <Categories title="Originales de Platzi Video">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default App
